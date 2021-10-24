8fe7f0f5dd3222722f43068ed443f9a36beec8c2
elid venega <venegaelid@gmail.com>
2021-10-23 20:10:20 -0700

    Work on understanding code even better

--- animated-countdown/script.js
+++ animated-countdown/script.js
@@ -2,14 +2,14 @@
 const nums = document.querySelectorAll('.nums span')
 // this variable is for the div with the number's
 const counter = document.querySelector('.counter')
-// selects last div to replay animation
+// selects second div to replay animation
 const finalMessage = document.querySelector('.final')
 // this variable is for the button
 const replay = document.querySelector('#replay')
 
 // Why is this function been invoke at the top
 // makes sure animation runs
-// runs animation automatically
+// invoke's animation automatically
 runAnimation()
 
 function resetDOM() {
@@ -30,19 +30,27 @@ function runAnimation() {
   nums.forEach((num, idx) => {
     const nextToLast = nums.length - 1
 
-  //  try to understand this last part
-  // num target' all numbers
-  // The animationend event is fired when a CSS Animation has completed
+    //  try to understand this last part
+    // num target' all numbers
+    // The animationend event is fired when a CSS Animation has completed
     num.addEventListener('animationend', (e) => {
       if (e.animationName === 'goIn' && idx !== nextToLast) {
         // removes animation
         num.classList.remove('in')
         // add's text
         num.classList.add('out')
+
+        // add's animation
+        /* The animationName property returns the name of the animation, 
+            when an animation event occurs.
+            The name of the animation is the value of the animation-name CSS property.
+            This property is read-only. */
       } else if (e.animationName === 'goOut' && num.nextElementSibling) {
         num.nextElementSibling.classList.add('in')
       } else {
+        // first div with numbers
         counter.classList.add('hide')
+        // last div to replay animation
         finalMessage.classList.add('show')
       }
     })
@@ -53,5 +61,3 @@ replay.addEventListener('click', () => {
   resetDOM()
   runAnimation()
 })
-
-
